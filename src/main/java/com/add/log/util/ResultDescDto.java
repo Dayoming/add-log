4f3053ca010744a65126f70b7b81301a654c78e1
dayoung <jdayoung1204@naver.com>
2026-07-06 14:33:07 +0900

    refactor: Lombok @Getter, @Setter 사용

--- src/main/java/com/add/log/util/ResultDescDto.java
+++ src/main/java/com/add/log/util/ResultDescDto.java
@@ -1,58 +1,20 @@
 package com.add.log.util;
 
 import com.google.gson.GsonBuilder;
-import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class ResultDescDto {
     private String code = null;
     private String desc = null;
     private String type = null;
     private String status = null;
 
-    public ResultDescDto() {
-    }
-
     public String toString() {
         return (new GsonBuilder()).serializeNulls().create().toJson(this);
-    }
-
-    @Generated
-    public String getCode() {
-        return this.code;
-    }
-
-    @Generated
-    public String getDesc() {
-        return this.desc;
-    }
-
-    @Generated
-    public String getType() {
-        return this.type;
-    }
-
-    @Generated
-    public String getStatus() {
-        return this.status;
-    }
-
-    @Generated
-    public void setCode(final String code) {
-        this.code = code;
-    }
-
-    @Generated
-    public void setDesc(final String desc) {
-        this.desc = desc;
-    }
-
-    @Generated
-    public void setType(final String type) {
-        this.type = type;
-    }
-
-    @Generated
-    public void setStatus(final String status) {
-        this.status = status;
     }
 }
